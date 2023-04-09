396682a77f135aa842c872d033c1b6e48f5eb5aa
xiture9640 <xiture9640@naver.com>
2023-04-10 01:50:08 +0900

    Core 시스템 및 기반 인터페이스 추가
    Core 레이어를 나누어 다른 게임에서도 사용할 수 있도록 함

---
+++ Assets/Scripts/Core/ISystem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Core
+{
+    /// <summary>
+    /// 시스템 기본 구현 필요 사항
+    /// FIXME : 여기에 싱글톤 Instance getter를 넣을지...
+    /// </summary>
+    public interface ISystem : IDisposable
+    {
+        void Update();
+    }
+}

4765be0e3d9f91af07dd1f25fcaf46f0adca8333
xiture9640 <xiture9640@naver.com>
2023-04-10 01:54:28 +0900

    Game 베이스 코드 추가
    게임에서 제일 먼저 생성되고 제일 나중에 파괴되는 컴포넌트

---
+++ Assets/Scripts/Dpm/Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Core;
+using Core.Message;
+using UnityEngine;
+
+namespace Dpm
+{
+    public class Game : MonoBehaviour
+    {
+        private EventSystem _eventSystem;
+        private UpdateSystem _updateSystem;
+
+        private void Awake()
+        {
+            _eventSystem = EventSystem.Instance;
+            _updateSystem = UpdateSystem.Instance;
+        }
+
+        private void OnDestroy()
+        {
+            (_eventSystem as IDisposable).Dispose();
+            (_updateSystem as IDisposable).Dispose();
+        }
+
+        private void Update()
+        {
+            // FIXME : 순서는 바뀔 수 있음
+            (_eventSystem as ISystem).Update();
+            (_updateSystem as ISystem).Update();
+        }
+    }
+}

f2fbc43b736e86c280d0717035d21f07358985bb
xiture9640 <xiture9640@naver.com>
2023-04-11 07:42:04 +0900

    EventSystem에 Update시점에 맞춰 전달 혹은 브로드캐스팅되는 기능 추가 및 구조 개선 (검증 필요)

--- Assets/Scripts/Core/Message/EventSystem.cs
+++ Assets/Scripts/Core/Message/EventSystem.cs
@@ -4,125 +4,306 @@ using UnityEngine;
 
 namespace Core.Message
 {
-    /// <summary>
-    /// 메세지 시스템
-    /// </summary>
-    public class EventSystem : ISystem
-    {
-        public static EventSystem Instance { get; } = new EventSystem();
+	/// <summary>
+	/// 메세지 시스템
+	/// </summary>
+	public class EventSystem : ISystem
+	{
+		public static EventSystem Instance { get; } = new EventSystem();
 
-        public delegate bool Subscriber(Event e);
+		/// <summary>
+		/// 구독자(함수)의 형태 규정
+		/// </summary>
+		public delegate bool Subscriber(Event e);
 
-        // private struct IssuedEvent
-        // {
-        //     public Event Event;
-        //     public IEventListener Listener;
-        // }
+		/// <summary>
+		/// (Immediate가 아닌 메소드로) 발행된 이벤트들
+		/// </summary>
+		private struct EventRequest
+		{
+			public Event Event;
 
-        // private List<IssuedEvent> _issuedEvents;
+			/// <summary>
+			/// null이면 브로드캐스팅된 이벤트임
+			/// </summary>
+			public IEventListener Listener;
 
-        private Dictionary<Type, List<Subscriber>> _subscribers = new ();
+			/// <summary>
+			/// 이 이벤트가 브로드캐스팅된 것이면 true
+			/// </summary>
+			public bool IsPublished => ReferenceEquals(Listener, null);
+		}
 
-        void IDisposable.Dispose()
-        {
-            _subscribers?.Clear();
-            _subscribers = null;
-        }
+		/// <summary>
+		/// 구독인지 구독 해제인지 확인하기 위한 타입
+		/// </summary>
+		private enum SubscribeRequestType
+		{
+			Subscribe,
+			Unsubscribe
+		}
 
-        void ISystem.Update()
-        {
-            // TODO : _issuedEvents 처리
-        }
+		/// <summary>
+		/// 구독/구독 해제 요청 정보
+		/// </summary>
+		private struct SubscribeRequest
+		{
+			/// <summary>
+			/// 구독/구독해제할 이벤트의 타입
+			/// 이벤트의 타입은 항상 Core.Message.Event를 상속받고 있을 것이므로 (Subscribe를 통해 사용) 타입에 대한 제너릭은 정의하지 않음
+			/// </summary>
+			public Type EventType;
 
-        public void Subscribe<T>(Subscriber subscriber) where T : Event
-        {
-            var eventType = typeof(T);
+			/// <summary>
+			/// 구독/구독해제할 콜백
+			/// </summary>
+			public Subscriber Target;
 
-            if (!_subscribers.TryGetValue(eventType, out var subscribers))
-            {
-                subscribers = new List<Subscriber>();
+			/// <summary>
+			/// 구독인지 구독 해제인지
+			/// </summary>
+			public SubscribeRequestType RequestType;
+		}
 
-                _subscribers.Add(eventType, subscribers);
-            }
+		private List<SubscribeRequest> _subscribeRequests = new();
+
+		private Dictionary<Type, List<Subscriber>> _subscribers = new();
+
+		private List<EventRequest> _eventRequests = new();
+
+		void IDisposable.Dispose()
+		{
+			_eventRequests.Clear();
+			_subscribeRequests.Clear();
+			_subscribers.Clear();
+		}
+
+		void ISystem.Update()
+		{
+			// 구독 / 구독 해제 요청 일괄 처리
+			for (int i = 0; i < _subscribeRequests.Count; i++)
+			{
+				var request = _subscribeRequests[i];
+
+				if (request.RequestType == SubscribeRequestType.Subscribe)
+				{
+					if (!_subscribers.TryGetValue(request.EventType, out var subscribers))
+					{
+						subscribers = new List<Subscriber>();
+
+						_subscribers.Add(request.EventType, subscribers);
+					}
 
 #if UNITY_EDITOR
-            // 에디터에서는 중복 구독을 개발 단계에서 캐치할 수 있도록 에러 로그를 남김
-            // 빌드에서 체크하지 않는 이유는 List 순회에 비용이 크게 발생하기 때문
-            // FIXME : 빌드에서는 중복 구독이 발생한다고 하면 어떤 방식으로 트래킹해야 하는가?
-            if (subscribers.Contains(subscriber))
-            {
-                Debug.LogError($"{ subscriber } Has Subscribed { eventType } Already.");
-                return;
-            }
+					// 에디터에서는 중복 구독을 개발 단계에서 캐치할 수 있도록 에러 로그를 남김
+					// 빌드에서 체크하지 않는 이유는 List 순회에 비용이 크게 발생하기 때문
+					// FIXME : 빌드에서는 중복 구독이 발생한다고 하면 어떤 방식으로 트래킹해야 하는가?
+					if (subscribers.Contains(request.Target))
+					{
+						Debug.LogError($"{request.Target} Has Subscribed {request.EventType} Already.");
+						return;
+					}
 #endif
-            subscribers.Add(subscriber);
-        }
+					subscribers.Add(request.Target);
+				}
+				else
+				{
+					if (!_subscribers.TryGetValue(request.EventType, out var subscribers))
+					{
+						return;
+					}
 
-        public void Unsubscribe<T>(Subscriber subscriber) where T : Event
-        {
-            var eventType = typeof(T);
+					subscribers.Remove(request.Target);
 
-            if (!_subscribers.TryGetValue(eventType, out var subscribers))
-            {
-                return;
-            }
+					// 해당 Event에 대해 구독자가 더 이상 남아있지 않은 경우 리스트도 지워줌
+					// FIXME : Any() 등의 함수를 사용하는 것과 어떤 것이 다른가?
+					if (subscribers.Count == 0)
+					{
+						_subscribers.Remove(request.EventType);
+					}
+				}
+			}
 
-            subscribers.Remove(subscriber);
+			_subscribeRequests.Clear();
 
-            // 해당 Event에 대해 구독자가 더 이상 남아있지 않은 경우 리스트도 지워줌
-            // FIXME : Any() 등의 함수를 사용하는 것과 어떤 것이 다른가?
-            if (subscribers.Count == 0)
-            {
-                _subscribers.Remove(eventType);
-            }
-        }
+			// _eventRequests 처리
+			for (int i = 0; i < _eventRequests.Count; i++)
+			{
+				var request = _eventRequests[i];
 
-        public bool SendImmediate(IEventListener listener, Event e, bool disposeAfter = true)
-        {
-            bool consumed = listener.OnEvent(e);
+				if (request.IsPublished)
+				{
+					if (_subscribers.TryGetValue(request.Event.GetType(), out var subscribers))
+					{
+						for (int j = 0; j < subscribers.Count; j++)
+						{
+							bool hasUnsubscribedRequest = false;
 
-            if (disposeAfter)
-            {
-                e.Dispose();
-            }
+							// 구독해제를 했는데 메세지가 전달되는 현상을 막기 위함
+							// FIXME : 더 좋은 방법은 없을까?
+							for (int k = 0; k < _subscribeRequests.Count; k++)
+							{
+								if (_subscribeRequests[i].RequestType == SubscribeRequestType.Unsubscribe &&
+								    _subscribeRequests[i].Target == subscribers[i])
+								{
+									// 여기서 실시간으로 구독해제를 해버리면 구독 요청과의 싱크 문제가 있기 때문에 일단 패스
+									hasUnsubscribedRequest = true;
+									break;
+								}
+							}
 
-            return consumed;
-        }
+							if (!hasUnsubscribedRequest)
+							{
+								var consumed = subscribers[i].Invoke(request.Event);
 
-        public bool PublishImmediate(Event e, bool disposeAfter = true)
-        {
-            bool consumed = false;
+								if (consumed)
+								{
+									break;
+								}
+							}
+						}
+					}
+				}
+				else
+				{
+					request.Listener.OnEvent(request.Event);
+				}
 
-            if (_subscribers.TryGetValue(e.GetType(), out var subscribers))
-            {
-                for (int i = 0; i < subscribers.Count; i++)
-                {
-                    // 흡수된 경우에는 Return해버림
-                    consumed = subscribers[i](e);
+				request.Event.Dispose();
+			}
 
-                    if (consumed)
-                    {
-                        break;
-                    }
-                }
-            }
+			_eventRequests.Clear();
+		}
 
-            if (disposeAfter)
-            {
-                e.Dispose();
-            }
+		/// <summary>
+		/// 구독 요청
+		/// </summary>
+		/// <param name="target">구독할 대상(메서드). null을 넣지 말 것!!</param>
+		/// <typeparam name="T">구독할 Event 타입</typeparam>
+		public void Subscribe<T>(Subscriber target) where T : Event
+		{
+			_subscribeRequests.Add(new SubscribeRequest
+			{
+				RequestType = SubscribeRequestType.Subscribe,
+				Target = target,
+				EventType = typeof(T)
+			});
+		}
 
-            return consumed;
-        }
+		/// <summary>
+		/// 구독 해제 요청
+		/// </summary>
+		/// <param name="target">구독 해제할 대상(메서드)</param>
+		/// <typeparam name="T">구독 해제할 Event 타입</typeparam>
+		public void Unsubscribe<T>(Subscriber target) where T : Event
+		{
+			_subscribeRequests.Add(new SubscribeRequest
+			{
+				RequestType = SubscribeRequestType.Unsubscribe,
+				Target = target,
+				EventType = typeof(T)
+			});
+		}
 
-        // public void Send(Event e)
-        // {
-        //
-        // }
-        //
-        // public void Publish(Event e)
-        // {
-        //
-        // }
-    }
+		/// <summary>
+		/// 즉시 이벤트 전달
+		/// </summary>
+		/// <param name="listener">전달받을 객체</param>
+		/// <param name="e">전달할 이벤트</param>
+		/// <param name="disposeAfter">처리가 끝나고 Dispose를 해 줄 것인지. 최대한 기본값을 쓰는 것이 좋음</param>
+		/// <returns>흡수되었는지 여부</returns>
+		public bool SendImmediate(IEventListener listener, Event e, bool disposeAfter = true)
+		{
+			bool consumed = listener?.OnEvent(e) ?? false;
+
+			if (disposeAfter)
+			{
+				e.Dispose();
+			}
+
+			return consumed;
+		}
+
+		/// <summary>
+		/// 이벤트 즉시 브로드캐스팅
+		/// </summary>
+		/// <param name="e">브로드캐스팅할 이벤트</param>
+		/// <param name="disposeAfter">처리가 끝나고 Dispose를 해 줄 것인지. 최대한 기본값을 쓰는 것이 좋음</param>
+		/// <returns>흡수되었는지 여부</returns>
+		public bool PublishImmediate(Event e, bool disposeAfter = true)
+		{
+			bool consumed = false;
+
+			if (_subscribers.TryGetValue(e.GetType(), out var subscribers))
+			{
+				for (int i = 0; i < subscribers.Count; i++)
+				{
+					bool hasUnsubscribedRequest = false;
+
+					// 구독해제를 했는데 메세지가 전달되는 현상을 막기 위함
+					// FIXME : 더 좋은 방법은 없을까?
+					for (int k = 0; k < _subscribeRequests.Count; k++)
+					{
+						if (_subscribeRequests[i].RequestType == SubscribeRequestType.Unsubscribe &&
+						    _subscribeRequests[i].Target == subscribers[i])
+						{
+							// 여기서 실시간으로 구독해제를 해버리면 구독 요청과의 싱크 문제가 있기 때문에 일단 패스
+							hasUnsubscribedRequest = true;
+							break;
+						}
+					}
+
+					if (!hasUnsubscribedRequest)
+					{
+						// 흡수된 경우에는 Return해버림
+						consumed = subscribers[i].Invoke(e);
+
+						if (consumed)
+						{
+							break;
+						}
+					}
+				}
+			}
+
+			if (disposeAfter)
+			{
+				e.Dispose();
+			}
+
+			return consumed;
+		}
+
+		/// <summary>
+		/// EventSystem 업데이트 시점에 맞춰 이벤트 전달
+		/// </summary>
+		/// <param name="target">전달받을 객체</param>
+		/// <param name="e">전달할 이벤트</param>
+		public void Send(IEventListener target, Event e)
+		{
+			if (target == null)
+			{
+				return;
+			}
+
+			_eventRequests.Add(new EventRequest
+			{
+				Event = e,
+				Listener = target
+			});
+		}
+
+		/// <summary>
+		/// EventSystem 업데이트 시점에 맞춰 이벤트 브로드캐스팅
+		/// </summary>
+		/// <param name="e">브로드캐스팅할 이벤트</param>
+		public void Publish(Event e)
+		{
+			_eventRequests.Add(new EventRequest
+			{
+				Event = e,
+				Listener = null
+			});
+		}
+	}
 }

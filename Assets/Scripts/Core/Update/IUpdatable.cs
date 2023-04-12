0ab1652ae9320e16f817f4604c843f09d01ecade
xiture9640 <xiture9640@naver.com>
2023-04-12 15:46:39 +0900

    UpdateSystem 구현
    게임이 무겁지 않을 것으로 예상되어서 간단하게 UpdatePriority에 대한 체크를 전부 O(N)에 동작하도록 구현

---
+++ Assets/Scripts/Core/Update/IUpdatable.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Update
+{
+	/// <summary>
+	/// MonoBehaviour의 Update를 사용하는 대신 이 인스턴스를 구현해서 쓸 것
+	/// Game.cs는 예외
+	/// </summary>
+	public interface IUpdatable
+	{
+		void UpdateFrame(float dt);
+	}
+}
--- Assets/Scripts/Dpm/Game.cs
+++ Assets/Scripts/Dpm/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Core;
 using Core.Message;
+using Core.Update;
 using UnityEngine;
 
 namespace Dpm
@@ -19,7 +20,10 @@ namespace Dpm
 			_instance = this;
 
 			_eventSystem = EventSystem.Instance;
-			_updateSystem = UpdateSystem.Instance;
+			_updateSystem = new();
+
+			// 씬 이동이나 씬 전체 해제 등의 행위로 날아가지 않도록 세팅
+			DontDestroyOnLoad(this);
 		}
 
 		private void OnDestroy()
@@ -30,9 +34,11 @@ namespace Dpm
 
 		private void Update()
 		{
-			// FIXME : 순서는 바뀔 수 있음
-			(_eventSystem as ISystem).Update();
-			(_updateSystem as ISystem).Update();
+			var dt = Time.deltaTime;
+
+			// FIXME : 순서 지정 필요
+			(_updateSystem as IUpdatable).UpdateFrame(dt);
+			_eventSystem.ProcessEventRequests();
 		}
 	}
 }

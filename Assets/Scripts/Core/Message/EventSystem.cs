dd86e9b639e04fb6f9ac263f48bf45594d55bd7d
xiture9640 <xiture9640@naver.com>
2023-04-12 11:50:13 +0900

    EventSystem 구독/구독 해제 시점을 Update가 아닌 즉시 실행으로 변경

--- Assets/Scripts/Core/Message/EventSystem.cs
+++ Assets/Scripts/Core/Message/EventSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Core.Message
@@ -9,7 +10,7 @@ namespace Core.Message
 	/// </summary>
 	public class EventSystem : ISystem
 	{
-		public static EventSystem Instance { get; } = new EventSystem();
+		public static EventSystem Instance { get; } = new();
 
 		/// <summary>
 		/// 구독자(함수)의 형태 규정
@@ -43,89 +44,25 @@ namespace Core.Message
 			Unsubscribe
 		}
 
-		/// <summary>
-		/// 구독/구독 해제 요청 정보
-		/// </summary>
-		private struct SubscribeRequest
-		{
-			/// <summary>
-			/// 구독/구독해제할 이벤트의 타입
-			/// 이벤트의 타입은 항상 Core.Message.Event를 상속받고 있을 것이므로 (Subscribe를 통해 사용) 타입에 대한 제너릭은 정의하지 않음
-			/// </summary>
-			public Type EventType;
+		private const int SubscriberWalkerInvalidValue = -1;
+		private int _subscriberWalker = SubscriberWalkerInvalidValue;
 
-			/// <summary>
-			/// 구독/구독해제할 콜백
-			/// </summary>
-			public Subscriber Target;
+		private bool IsSubscribeWalking => _subscriberWalker == SubscriberWalkerInvalidValue;
 
-			/// <summary>
-			/// 구독인지 구독 해제인지
-			/// </summary>
-			public SubscribeRequestType RequestType;
-		}
+		private Type _subscriberWalkingEventType;
 
-		private List<SubscribeRequest> _subscribeRequests = new();
+		private readonly Dictionary<Type, List<Subscriber>> _subscribers = new();
 
-		private Dictionary<Type, List<Subscriber>> _subscribers = new();
-
-		private List<EventRequest> _eventRequests = new();
+		private readonly List<EventRequest> _eventRequests = new();
 
 		void IDisposable.Dispose()
 		{
 			_eventRequests.Clear();
-			_subscribeRequests.Clear();
 			_subscribers.Clear();
 		}
 
 		void ISystem.Update()
 		{
-			// 구독 / 구독 해제 요청 일괄 처리
-			for (int i = 0; i < _subscribeRequests.Count; i++)
-			{
-				var request = _subscribeRequests[i];
-
-				if (request.RequestType == SubscribeRequestType.Subscribe)
-				{
-					if (!_subscribers.TryGetValue(request.EventType, out var subscribers))
-					{
-						subscribers = new List<Subscriber>();
-
-						_subscribers.Add(request.EventType, subscribers);
-					}
-
-#if UNITY_EDITOR
-					// 에디터에서는 중복 구독을 개발 단계에서 캐치할 수 있도록 에러 로그를 남김
-					// 빌드에서 체크하지 않는 이유는 List 순회에 비용이 크게 발생하기 때문
-					// FIXME : 빌드에서는 중복 구독이 발생한다고 하면 어떤 방식으로 트래킹해야 하는가?
-					if (subscribers.Contains(request.Target))
-					{
-						Debug.LogError($"{request.Target} Has Subscribed {request.EventType} Already.");
-						return;
-					}
-#endif
-					subscribers.Add(request.Target);
-				}
-				else
-				{
-					if (!_subscribers.TryGetValue(request.EventType, out var subscribers))
-					{
-						return;
-					}
-
-					subscribers.Remove(request.Target);
-
-					// 해당 Event에 대해 구독자가 더 이상 남아있지 않은 경우 리스트도 지워줌
-					// FIXME : Any() 등의 함수를 사용하는 것과 어떤 것이 다른가?
-					if (subscribers.Count == 0)
-					{
-						_subscribers.Remove(request.EventType);
-					}
-				}
-			}
-
-			_subscribeRequests.Clear();
-
 			// _eventRequests 처리
 			for (int i = 0; i < _eventRequests.Count; i++)
 			{
@@ -133,36 +70,24 @@ namespace Core.Message
 
 				if (request.IsPublished)
 				{
-					if (_subscribers.TryGetValue(request.Event.GetType(), out var subscribers))
+					_subscriberWalkingEventType = request.Event.GetType();
+
+					if (_subscribers.TryGetValue(_subscriberWalkingEventType, out var subscribers))
 					{
-						for (int j = 0; j < subscribers.Count; j++)
+						for (_subscriberWalker = 0; _subscriberWalker < subscribers.Count; _subscriberWalker++)
 						{
-							bool hasUnsubscribedRequest = false;
+							var consumed = subscribers[_subscriberWalker].Invoke(request.Event);
 
-							// 구독해제를 했는데 메세지가 전달되는 현상을 막기 위함
-							// FIXME : 더 좋은 방법은 없을까?
-							for (int k = 0; k < _subscribeRequests.Count; k++)
+							if (consumed)
 							{
-								if (_subscribeRequests[i].RequestType == SubscribeRequestType.Unsubscribe &&
-								    _subscribeRequests[i].Target == subscribers[i])
-								{
-									// 여기서 실시간으로 구독해제를 해버리면 구독 요청과의 싱크 문제가 있기 때문에 일단 패스
-									hasUnsubscribedRequest = true;
-									break;
-								}
-							}
-
-							if (!hasUnsubscribedRequest)
-							{
-								var consumed = subscribers[i].Invoke(request.Event);
-
-								if (consumed)
-								{
-									break;
-								}
+								break;
 							}
 						}
+
+						_subscriberWalker = SubscriberWalkerInvalidValue;
 					}
+
+					_subscriberWalkingEventType = null;
 				}
 				else
 				{
@@ -182,12 +107,26 @@ namespace Core.Message
 		/// <typeparam name="T">구독할 Event 타입</typeparam>
 		public void Subscribe<T>(Subscriber target) where T : Event
 		{
-			_subscribeRequests.Add(new SubscribeRequest
+			var eventType = typeof(T);
+
+			if (!_subscribers.TryGetValue(eventType, out var subscribers))
 			{
-				RequestType = SubscribeRequestType.Subscribe,
-				Target = target,
-				EventType = typeof(T)
-			});
+				subscribers = new List<Subscriber>();
+
+				_subscribers.Add(eventType, subscribers);
+			}
+
+#if UNITY_EDITOR
+			// 에디터에서는 중복 구독을 개발 단계에서 캐치할 수 있도록 에러 로그를 남김
+			// 빌드에서 체크하지 않는 이유는 List 순회에 비용이 크게 발생하기 때문
+			// FIXME : 빌드에서는 중복 구독이 발생한다고 하면 어떤 방식으로 트래킹해야 하는가?
+			if (subscribers.Contains(target))
+			{
+				Debug.LogError($"{target} Has Subscribed {eventType} Already.");
+				return;
+			}
+#endif
+			subscribers.Add(target);
 		}
 
 		/// <summary>
@@ -197,12 +136,36 @@ namespace Core.Message
 		/// <typeparam name="T">구독 해제할 Event 타입</typeparam>
 		public void Unsubscribe<T>(Subscriber target) where T : Event
 		{
-			_subscribeRequests.Add(new SubscribeRequest
+			var eventType = typeof(T);
+
+			if (!_subscribers.TryGetValue(eventType, out var subscribers))
 			{
-				RequestType = SubscribeRequestType.Unsubscribe,
-				Target = target,
-				EventType = typeof(T)
-			});
+				return;
+			}
+
+			for (int i = 0; i < subscribers.Count; i++)
+			{
+				if (subscribers[i] != target)
+					continue;
+
+				// 해당 타입에 대해서 브로드캐스팅 중이면 인덱스 검사해서 조정해줌
+				if (eventType == _subscriberWalkingEventType && IsSubscribeWalking && i > _subscriberWalker)
+				{
+					_subscriberWalker--;
+				}
+
+				subscribers.RemoveAt(i);
+
+				break;
+			}
+
+			// 해당 Event에 대해 구독자가 더 이상 남아있지 않은 경우 리스트도 지워줌
+			// FIXME : Any() 등의 함수를 사용하는 것과 어떤 것이 다른가?
+			if (subscribers.Count == 0)
+			{
+				// 브로드캐스팅 문맥 상에서 동작하는 상황이라도, 레퍼런스는 해당 문맥이 들고있고 바로 파괴되는 것도 아니기 때문에 _subscribers에서는 지워도 됨
+				_subscribers.Remove(eventType);
+			}
 		}
 
 		/// <summary>
@@ -214,7 +177,16 @@ namespace Core.Message
 		/// <returns>흡수되었는지 여부</returns>
 		public bool SendImmediate(IEventListener listener, Event e, bool disposeAfter = true)
 		{
-			bool consumed = listener?.OnEvent(e) ?? false;
+			bool consumed = false;
+
+			if (listener != null)
+			{
+				consumed = listener.OnEvent(e);
+			}
+			else
+			{
+				Debug.LogError($"Send an { e } to null target");
+			}
 
 			if (disposeAfter)
 			{
@@ -234,37 +206,25 @@ namespace Core.Message
 		{
 			bool consumed = false;
 
-			if (_subscribers.TryGetValue(e.GetType(), out var subscribers))
+			_subscriberWalkingEventType = e.GetType();
+
+			if (_subscribers.TryGetValue(_subscriberWalkingEventType, out var subscribers))
 			{
-				for (int i = 0; i < subscribers.Count; i++)
+				for (_subscriberWalker = 0; _subscriberWalker < subscribers.Count; _subscriberWalker++)
 				{
-					bool hasUnsubscribedRequest = false;
+					// 흡수된 경우에는 Return해버림
+					consumed = subscribers[_subscriberWalker].Invoke(e);
 
-					// 구독해제를 했는데 메세지가 전달되는 현상을 막기 위함
-					// FIXME : 더 좋은 방법은 없을까?
-					for (int k = 0; k < _subscribeRequests.Count; k++)
+					if (consumed)
 					{
-						if (_subscribeRequests[i].RequestType == SubscribeRequestType.Unsubscribe &&
-						    _subscribeRequests[i].Target == subscribers[i])
-						{
-							// 여기서 실시간으로 구독해제를 해버리면 구독 요청과의 싱크 문제가 있기 때문에 일단 패스
-							hasUnsubscribedRequest = true;
-							break;
-						}
-					}
-
-					if (!hasUnsubscribedRequest)
-					{
-						// 흡수된 경우에는 Return해버림
-						consumed = subscribers[i].Invoke(e);
-
-						if (consumed)
-						{
-							break;
-						}
+						break;
 					}
 				}
+
+				_subscriberWalker = SubscriberWalkerInvalidValue;
 			}
+
+			_subscriberWalkingEventType = null;
 
 			if (disposeAfter)
 			{
@@ -277,20 +237,24 @@ namespace Core.Message
 		/// <summary>
 		/// EventSystem 업데이트 시점에 맞춰 이벤트 전달
 		/// </summary>
-		/// <param name="target">전달받을 객체</param>
+		/// <param name="listener">전달받을 객체</param>
 		/// <param name="e">전달할 이벤트</param>
-		public void Send(IEventListener target, Event e)
+		public void Send(IEventListener listener, Event e)
 		{
-			if (target == null)
+			if (listener != null)
 			{
-				return;
+				_eventRequests.Add(new EventRequest
+				{
+					Event = e,
+					Listener = listener
+				});
 			}
-
-			_eventRequests.Add(new EventRequest
+			else
 			{
-				Event = e,
-				Listener = target
-			});
+				Debug.LogError($"Send an { e } to null target");
+
+				e.Dispose();
+			}
 		}
 
 		/// <summary>

9d882ddf4352e218054c64925f774d337764dcec
xiture9640 <xiture9640@naver.com>
2023-04-22 17:56:57 +0900

    씬 전용 인터페이스와 씬 매니저 추가

---
+++ Assets/Scripts/Dpm/IScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+namespace Dpm
+{
+	public interface IScene
+	{
+		/// <summary>
+		/// 로딩 루틴
+		/// </summary>
+		IEnumerator LoadAsync();
+
+		/// <summary>
+		/// 씬 진입
+		/// </summary>
+		void Enter();
+
+		/// <summary>
+		/// 씬 탈출
+		/// </summary>
+		void Exit();
+	}
+}
---
+++ Assets/Scripts/Dpm/SceneManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Dpm
+{
+	public class SceneManager
+	{
+		public static SceneManager Instance { get; } = new();
+
+		private IScene _currentScene;
+
+		public IEnumerator EnterScene(IScene nextScene)
+		{
+			if (_currentScene != null)
+			{
+				Debug.LogError($"Previous scene [{ _currentScene }] is still exist");
+				yield break;
+			}
+
+			_currentScene = nextScene;
+			yield return nextScene.LoadAsync();
+
+			nextScene.Enter();
+		}
+
+		public void ExitCurrentScene()
+		{
+			_currentScene?.Exit();
+			_currentScene = null;
+		}
+	}
+}

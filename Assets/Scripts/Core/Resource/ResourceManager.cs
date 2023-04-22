1a9ff131983058509787889f26b4fac6ba0ff2f0
xiture9640 <xiture9640@naver.com>
2023-04-22 17:58:48 +0900

    리소스 매니저 추가
    일단은 프리팹만 관리하도록 했음. 코드 상에서 그 외의 리소스를 커스텀하게 로드해야 할 일은 없어야 할 듯

---
+++ Assets/Scripts/Core/Resource/ResourceManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Resource
+{
+	public class ResourceManager
+	{
+		private readonly string _prefabLootPath = "Prefabs";
+
+		private readonly Dictionary<string, GameObject> _prefabs = new();
+
+		public static ResourceManager Instance => _instance;
+
+		private static ResourceManager _instance;
+
+		public ResourceManager(string prefabLootPath = null)
+		{
+			_instance = this;
+
+			if (prefabLootPath != null)
+			{
+				_prefabLootPath = prefabLootPath;
+			}
+		}
+
+		public IEnumerator LoadAsync(string path, Action<GameObject> doneCallback)
+		{
+			if (!_prefabs.ContainsKey(path))
+			{
+				var loadReq = Resources.LoadAsync($"{_prefabLootPath}/{path}");
+
+				yield return loadReq;
+
+				if (loadReq.asset is GameObject assetGo)
+				{
+					_prefabs.Add(path, assetGo);
+				}
+#if UNITY_EDITOR
+				else
+				{
+					Debug.LogError($"{ path } load failed.");
+
+					yield break;
+				}
+#endif
+			}
+
+			doneCallback.Invoke(_prefabs[path]);
+		}
+	}
+}

5ea1be0ce628e96d8ee66cb74956fa6876e78b0e
xiture9640 <xiture9640@naver.com>
2023-05-07 05:37:29 +0900

    리소스 및 코루틴 매니저 인터페이스 추가

---
+++ Assets/Scripts/Core/Interface/ICoroutineManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+
+namespace Core.Interface
+{
+	public interface ICoroutineManager : IDisposable
+	{
+		Guid StartCoroutine(IEnumerator enumerator);
+
+		void StopCoroutine(Guid guid);
+	}
+}

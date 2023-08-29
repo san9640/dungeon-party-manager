fe68d0357dd247fd8126d090777a621954fb1308
xiture9640 <xiture9640@naver.com>
2023-08-29 22:36:04 +0900

    ScreenFade 관련 이벤트 추가

---
+++ Assets/Scripts/Dpm/Common/Event/ScreenFadeInEndEvent.cs
@@ -0,0 +1,24 @@
+﻿using Dpm.Utility.Event;
+
+namespace Dpm.Common.Event
+{
+	public class ScreenFadeInEndEvent : PooledEvent<ScreenFadeInEndEvent>
+	{
+		public object Requester { get; private set; }
+
+		public override void Dispose()
+		{
+			Requester = null;
+
+			base.Dispose();
+		}
+
+		public static ScreenFadeInEndEvent Create(object requester)
+		{
+			var e = Pool.GetOrCreate();
+			e.Requester = requester;
+
+			return e;
+		}
+	}
+}
---
+++ Assets/Scripts/Dpm/Common/Event/ScreenFadeOutStartEvent.cs
@@ -0,0 +1,24 @@
+﻿using Dpm.Utility.Event;
+
+namespace Dpm.Common.Event
+{
+	public class ScreenFadeOutStartEvent : PooledEvent<ScreenFadeOutStartEvent>
+	{
+		public object Requester { get; private set; }
+
+		public override void Dispose()
+		{
+			Requester = null;
+
+			base.Dispose();
+		}
+
+		public static ScreenFadeOutStartEvent Create(object requester)
+		{
+			var e = Pool.GetOrCreate();
+			e.Requester = requester;
+
+			return e;
+		}
+	}
+}

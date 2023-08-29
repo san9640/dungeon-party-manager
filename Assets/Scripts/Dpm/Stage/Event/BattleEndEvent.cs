867fa162cc0d1ed80c41da1360cb312bd42ce363
xiture9640 <xiture9640@naver.com>
2023-08-29 22:41:38 +0900

    BattleEndEvent 추가

---
+++ Assets/Scripts/Dpm/Stage/Event/BattleEndEvent.cs
@@ -0,0 +1,25 @@
+﻿using Dpm.Stage.Unit;
+using Dpm.Utility.Event;
+
+namespace Dpm.Stage.Event
+{
+	public class BattleEndEvent : PooledEvent<BattleEndEvent>
+	{
+		public UnitRegion WonPartyRegion { get; private set; }
+
+		public override void Dispose()
+		{
+			WonPartyRegion = UnitRegion.Neutral;
+
+			base.Dispose();
+		}
+
+		public static BattleEndEvent Create(UnitRegion wonPartyRegion)
+		{
+			var e = Pool.GetOrCreate();
+			e.WonPartyRegion = wonPartyRegion;
+
+			return e;
+		}
+	}
+}

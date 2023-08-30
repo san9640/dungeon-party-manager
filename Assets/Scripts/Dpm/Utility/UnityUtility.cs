2b27ba88b64becabfb1b4441bcd887f282e2c399
xiture9640 <xiture9640@naver.com>
2023-08-31 02:27:22 +0900

    UnityUtility 추가

---
+++ Assets/Scripts/Dpm/Utility/UnityUtility.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Dpm.Utility
+{
+	public static class UnityUtility
+	{
+		public static bool IsDestroyedOrNull(this Object obj)
+		{
+			return ReferenceEquals(obj, null);
+		}
+	}
+}

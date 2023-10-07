7b38a065ac3b2de6890c95d13f99e6bf11eb3fcd
xiture9640 <xiture9640@naver.com>
2023-10-08 03:44:25 +0900

    UnitRegion 파일 분리 및 Extension 추가

--- Assets/Scripts/Dpm/Stage/Unit/IUnit.cs
+++ Assets/Scripts/Dpm/Stage/Unit/IUnit.cs
@@ -6,16 +6,6 @@ using UnityEngine;
 
 namespace Dpm.Stage.Unit
 {
-	[Flags]
-	public enum UnitRegion
-	{
-		None = 0,
-		Ally = 1 << 0,
-		Enemy = 1 << 1,
-		Neutral = 1 << 2,
-		Creature = Ally | Enemy,
-		All = Ally | Enemy | Neutral,
-	}
 
 	public interface IUnit : ICustomCollider, IEventListener
 	{
---
+++ Assets/Scripts/Dpm/Stage/Unit/UnitRegion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Dpm.Stage.Unit
+{
+	[Flags]
+	public enum UnitRegion
+	{
+		None = 0,
+		Ally = 1 << 0,
+		Enemy = 1 << 1,
+		Neutral = 1 << 2,
+		Creature = Ally | Enemy,
+		All = Ally | Enemy | Neutral,
+	}
+
+	public static class UnitRegionExtensions
+	{
+		public static bool IsOppositeParty(this UnitRegion me, UnitRegion other)
+		{
+			if ((me & other) != UnitRegion.None)
+			{
+				return false;
+			}
+
+			return (me | other) == UnitRegion.Creature;
+		}
+	}
+}

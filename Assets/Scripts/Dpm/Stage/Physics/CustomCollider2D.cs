23b66bbd6e77e9612147325f604624a419d9d984
xiture9640 <xiture9640@naver.com>
2023-10-08 03:48:20 +0900

    2D 충돌체용 컴포넌트 추가

---
+++ Assets/Scripts/Dpm/Stage/Physics/CustomCollider2D.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Dpm.Stage.Physics
+{
+	public class CustomCollider2D : MonoBehaviour
+	{
+		public Bounds2D bounds;
+
+#if UNITY_EDITOR
+		private void OnDrawGizmos()
+		{
+			Gizmos.color = Color.green;
+
+			Gizmos.DrawWireCube(transform.position, bounds.Size);
+		}
+#endif
+	}
+}
